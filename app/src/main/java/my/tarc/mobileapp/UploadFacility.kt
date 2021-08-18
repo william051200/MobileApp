3dd4f961b4873e275896342da3bd57c686d1eff5
williamchongwx <chongwx-pm18@student.tarc.edu.my>
2021-08-18 08:24:23 +0800

    - added upload & save icon on button - added upload facility ui - Modify the upload facility ui (added city,state & zipcode on address section) & (added start & close time on operation hours section)

---
+++ app/src/main/java/my/tarc/mobileapp/UploadFacility.kt
@@ -0,0 +1,11 @@
+package my.tarc.mobileapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UploadFacility : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_upload_facility)
+    }
+}
