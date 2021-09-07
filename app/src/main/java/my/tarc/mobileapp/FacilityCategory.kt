6c73bed033e3c5b061e45e288b48fad159b54a6c
williamchongwx <chongwx-pm18@student.tarc.edu.my>
2021-09-07 16:03:36 +0800

    - completed recreate 3 fragment and ui - refactor id - added icon (toilet, park, bus, upload)

--- app/src/main/java/my/tarc/mobileapp/FacilityCategory.kt
+++ app/src/main/java/my/tarc/mobileapp/FacilityCategory.kt
@@ -1,4 +1,24 @@
 package my.tarc.mobileapp
 
-class FacilityCategory {
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class FacilityCategory : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_facility_category, container, false)
+    }
+
 }
