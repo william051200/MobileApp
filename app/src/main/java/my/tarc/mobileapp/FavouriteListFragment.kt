ca02b0588788688fddc44bb59e5c2900fd85d71d
Asus <williamng0512@gmail.com>
2021-09-13 00:13:12 +0800

    edited sort button to spinner, added code for spinner, added open dialog code in favourite list

--- app/src/main/java/my/tarc/mobileapp/FavouriteListFragment.kt
+++ app/src/main/java/my/tarc/mobileapp/FavouriteListFragment.kt
@@ -1,9 +1,11 @@
 package my.tarc.mobileapp
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import my.tarc.mobileapp.databinding.FragmentFavouriteListBinding
@@ -12,6 +14,9 @@ class FavouriteListFragment : Fragment() {
 
     private var _binding: FragmentFavouriteListBinding? = null
     private val binding get() = _binding!!
+
+    private var sort: Int = binding.favouriteListSpinnerSort.selectedItemPosition
+    private lateinit var filter: Array<String>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,11 +30,8 @@ class FavouriteListFragment : Fragment() {
         super.onCreate(savedInstanceState)
         updateFacilityList()
 
-        // Open sort facility dialog
-        binding.favouriteListBtnSort.setOnClickListener { }
-
         // Open filter dialog
-        binding.favouriteListBtnFilter.setOnClickListener { }
+        binding.favouriteListBtnFilter.setOnClickListener { openFilterDialog() }
 
         // Navigate to user profile page
         binding.favouriteListBtnMyProfile.setOnClickListener {
@@ -42,8 +44,36 @@ class FavouriteListFragment : Fragment() {
     private fun updateFacilityList() {
         val facilityAdapter = FacilityAdapter()
 
+        if (sort == 0) {
+            // Sort ascending
+        } else if (sort == 1) {
+            // Sort descending
+        } else if (sort == 2) {
+            // Sort nearest
+        } else if (sort == 3) {
+            // Sort furthest
+        }
+
+
         // need view model
 //        facilityAdapter.setFacility(facilityViewModel.facilityList)
         binding.favouriteListRecycleView.adapter = facilityAdapter
+    }
+
+    private fun openFilterDialog() {
+        val view = View.inflate(this.context, R.layout.fragment_dialog_filter, null)
+
+        val builder = AlertDialog.Builder(this.context)
+        builder.setView(view)
+
+        val dialog = builder.create()
+        dialog.show()
+        dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+
+        val btnOk: Button = view.findViewById(R.id.filterDialog_btnApply)
+        btnOk.setOnClickListener {
+            // get filter value in array
+            dialog.dismiss()
+        }
     }
 }
