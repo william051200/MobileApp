ab9004ce953e19b12a2dd92049047be23216ce6f
Asus <williamng0512@gmail.com>
2021-09-13 00:18:25 +0800

    change sort button to spinner, added code to open filter dialog button in admin facility list

--- app/src/main/java/my/tarc/mobileapp/FacilityListAdminFragment.kt
+++ app/src/main/java/my/tarc/mobileapp/FacilityListAdminFragment.kt
@@ -1,16 +1,22 @@
 package my.tarc.mobileapp
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import my.tarc.mobileapp.databinding.FragmentFacilityListAdminBinding
 
 class FacilityListAdminFragment : Fragment() {
 
     private var _binding: FragmentFacilityListAdminBinding? = null
     private val binding get() = _binding!!
+
+    private var sort: Int = binding.facilityListAdminSpinnerSort.selectedItemPosition
+    private lateinit var filter: Array<String>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,6 +28,47 @@ class FacilityListAdminFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        updateFacilityList()
+
+        // Open filter dialog
+        binding.facilityListAdminBtnFilter.setOnClickListener { openFilterDialog() }
+
     }
 
+    // Update the facility list recycle view
+    private fun updateFacilityList() {
+        val facilityAdapter = FacilityAdapter()
+
+        if (sort == 0) {
+            // Sort ascending
+        } else if (sort == 1) {
+            // Sort descending
+        } else if (sort == 2) {
+            // Sort nearest
+        } else if (sort == 3) {
+            // Sort furthest
+        }
+
+
+        // need view model
+//        facilityAdapter.setFacility(facilityViewModel.facilityList)
+        binding.facilityListAdminRecycleView.adapter = facilityAdapter
+    }
+
+    private fun openFilterDialog() {
+        val view = View.inflate(this.context, R.layout.fragment_dialog_filter, null)
+
+        val builder = AlertDialog.Builder(this.context)
+        builder.setView(view)
+
+        val dialog = builder.create()
+        dialog.show()
+        dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+
+        val btnOk: Button = view.findViewById(R.id.filterDialog_btnApply)
+        btnOk.setOnClickListener {
+            // get filter value in array
+            dialog.dismiss()
+        }
+    }
 }
