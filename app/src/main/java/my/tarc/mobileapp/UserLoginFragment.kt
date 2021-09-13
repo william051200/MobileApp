ac415c1d06b9920fe1bb71e68d6ea5814899a317
Asus <williamng0512@gmail.com>
2021-09-13 11:26:51 +0800

    added navigation function to login button, edited id to txt field and button in login page

--- app/src/main/java/my/tarc/mobileapp/UserLoginFragment.kt
+++ app/src/main/java/my/tarc/mobileapp/UserLoginFragment.kt
@@ -1,34 +1,32 @@
 package my.tarc.mobileapp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import my.tarc.mobileapp.databinding.FragmentLoginPageBinding
-
 
 class UserLoginFragment : Fragment() {
     private var _binding: FragmentLoginPageBinding? = null
     private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-
-
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login_page, container, false)
-
-
+        _binding = FragmentLoginPageBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
+        // Navigate to user profile page
+        binding.loginUserBtnLogin.setOnClickListener {
+            findNavController().navigate(R.id.action_userLoginFragment_to_facilityCategory)
+        }
+    }
 
 }
