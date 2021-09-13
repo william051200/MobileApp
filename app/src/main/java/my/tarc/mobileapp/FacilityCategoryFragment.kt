9da02c314eed9b7668ae3a56f18893588011eaaa
Asus <williamng0512@gmail.com>
2021-09-13 11:37:15 +0800

    added navigation from facility category to favourite facility

--- app/src/main/java/my/tarc/mobileapp/FacilityCategoryFragment.kt
+++ app/src/main/java/my/tarc/mobileapp/FacilityCategoryFragment.kt
@@ -1,24 +1,32 @@
 package my.tarc.mobileapp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import my.tarc.mobileapp.databinding.FragmentFacilityCategoryBinding
 
 class FacilityCategoryFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private var _binding: FragmentFacilityCategoryBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_facility_category, container, false)
+        _binding = FragmentFacilityCategoryBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Navigate to favourite category page
+        binding.btnFavoriteCategory.setOnClickListener {
+            findNavController().navigate(R.id.action_facilityCategory_to_favouriteListFragment)
+        }
+    }
 }
