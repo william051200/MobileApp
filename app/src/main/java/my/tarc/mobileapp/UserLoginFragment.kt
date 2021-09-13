515c30ab709f04fc074dbf1c6e3b0f834b79d1e2
ooijp-pm18@student.tarc.edu.my <ooijp-pm18@student.tarc.edu.my>
2021-09-13 16:07:59 +0800

    feat: fix nav graph

--- app/src/main/java/my/tarc/mobileapp/UserLoginFragment.kt
+++ app/src/main/java/my/tarc/mobileapp/UserLoginFragment.kt
@@ -6,17 +6,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import my.tarc.mobileapp.databinding.FragmentLoginPageBinding
+import my.tarc.mobileapp.databinding.FragmentUserLoginBinding
 
 class UserLoginFragment : Fragment() {
-    private var _binding: FragmentLoginPageBinding? = null
+    private var _binding: FragmentUserLoginBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentLoginPageBinding.inflate(inflater, container, false)
+        _binding = FragmentUserLoginBinding.inflate(inflater, container, false)
         return binding.root
     }
 
