2e237fdc59cfa072d89f9066a7dd90cf87891dd8
williamchongwx <chongwx-pm18@student.tarc.edu.my>
2021-09-14 13:12:50 +0800

    link button between UserProfile and FacilityCategory

--- app/src/main/java/my/tarc/mobileapp/UserProfileFragment.kt
+++ app/src/main/java/my/tarc/mobileapp/UserProfileFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import my.tarc.mobileapp.databinding.FragmentUserProfileBinding
 
 
@@ -23,5 +24,24 @@ class UserProfileFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Navigate to Facility Category
+        binding.userProfileBtnFacilityCategory.setOnClickListener {
+            findNavController().navigate(R.id.action_userProfile_to_facilityCategory)
+        }
+
+        // Navigate to Facility Category
+        binding.btnProfileCancel.setOnClickListener {
+            findNavController().navigate(R.id.action_userProfile_to_facilityCategory)
+        }
+
+        //Navigate to Facility Category
+        binding.btnProfileSave.setOnClickListener {
+            findNavController().navigate(R.id.action_userProfile_to_facilityCategory)
+        }
     }
 }
