95bdb76c4f0eafad4f3782813cd77c71bb699dad
Asus <williamng0512@gmail.com>
2021-09-15 20:43:57 +0800

    edited facility class

--- app/src/main/java/my/tarc/mobileapp/facility/Facility.kt
+++ app/src/main/java/my/tarc/mobileapp/facility/Facility.kt
@@ -5,7 +5,10 @@ import android.graphics.Bitmap
 class Facility(
     val id: String,
     val name: String,
-    val address: String,
+    val address_street: String,
+    val address_postcode: String,
+    val address_city: String,
+    val address_state: String,
     val category: String,
     val feedbacks: Array<String>,
     val okuFeature: String,
@@ -15,7 +18,22 @@ class Facility(
     val status: String
 ) {
     var picture: Bitmap? = null
-    constructor(picture: Bitmap?, name: String) : this("", name, "", "", arrayOf(""), "", "", "", -1, ""){
+
+    constructor(picture: Bitmap?, name: String) : this(
+        "",
+        name,
+        "",
+        "",
+        "",
+        "",
+        "",
+        arrayOf(""),
+        "",
+        "",
+        "",
+        -1,
+        ""
+    ) {
         this.picture = picture
     }
 }
