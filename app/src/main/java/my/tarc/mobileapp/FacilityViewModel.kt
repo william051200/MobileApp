4737c6709f66d7decd16b6202cb6dc05e0d14001
ooijp-pm18@student.tarc.edu.my <ooijp-pm18@student.tarc.edu.my>
2021-09-15 17:49:23 +0800

    feat: update get image method

---
+++ app/src/main/java/my/tarc/mobileapp/FacilityViewModel.kt
@@ -0,0 +1,14 @@
+package my.tarc.mobileapp
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class FacilityViewModel : ViewModel() {
+    private val mutableSelectedFacility = MutableLiveData<Facility>()
+    val selectedFacility: LiveData<Facility> get() = mutableSelectedFacility
+
+    fun setFacility(facility: Facility) {
+        mutableSelectedFacility.value = facility
+    }
+}
