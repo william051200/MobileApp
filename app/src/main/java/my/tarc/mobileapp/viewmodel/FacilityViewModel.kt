1fb85f3d86e5549aea61387367cc0e3b9fae28d5
ooijp-pm18@student.tarc.edu.my <ooijp-pm18@student.tarc.edu.my>
2021-09-15 21:06:42 +0800

    feat: create view models

--- app/src/main/java/my/tarc/mobileapp/viewmodel/FacilityViewModel.kt
+++ app/src/main/java/my/tarc/mobileapp/viewmodel/FacilityViewModel.kt
@@ -4,12 +4,20 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import my.tarc.mobileapp.model.Facility
+import my.tarc.mobileapp.model.Feedback
 
 class FacilityViewModel : ViewModel() {
     private val mutableSelectedFacility = MutableLiveData<Facility>()
     val selectedFacility: LiveData<Facility> get() = mutableSelectedFacility
 
+    private val mutableSelectedFeedback = MutableLiveData<Feedback>()
+    val selectedFeedback: LiveData<Feedback> get() = mutableSelectedFeedback
+
     fun setFacility(facility: Facility) {
         mutableSelectedFacility.value = facility
+    }
+
+    fun setFeedback(feedback: Feedback) {
+        mutableSelectedFeedback.value = feedback
     }
 }
