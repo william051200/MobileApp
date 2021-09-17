4693c94e2d170738c6ffb58f66b650830f2a3ec1
ooijp-pm18@student.tarc.edu.my <ooijp-pm18@student.tarc.edu.my>
2021-09-17 15:14:18 +0800

    feat: change toolbar

--- app/src/main/java/my/tarc/mobileapp/facility/FacilityDetailsFragment.kt
+++ app/src/main/java/my/tarc/mobileapp/facility/FacilityDetailsFragment.kt
@@ -12,6 +12,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -55,6 +56,7 @@ class FacilityDetailsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentFacilityDetailsBinding.inflate(inflater, container, false)
+        (activity as AppCompatActivity?)!!.supportActionBar!!.title = "Facility Details"
         return binding.root
     }
 
