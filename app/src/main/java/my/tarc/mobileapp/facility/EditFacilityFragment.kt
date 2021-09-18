c58c80f1507141cd8085cc0a70aa95dc6b58caba
williamchongwx <chongwx-pm18@student.tarc.edu.my>
2021-09-18 15:42:35 +0800

    Added selected facility address state

--- app/src/main/java/my/tarc/mobileapp/facility/EditFacilityFragment.kt
+++ app/src/main/java/my/tarc/mobileapp/facility/EditFacilityFragment.kt
@@ -28,10 +28,6 @@ import my.tarc.mobileapp.R
 import my.tarc.mobileapp.databinding.FragmentEditFacilityBinding
 import my.tarc.mobileapp.viewmodel.FacilityViewModel
 import my.tarc.mobileapp.viewmodel.UserViewModel
-import android.widget.ArrayAdapter
-
-
-
 
 class EditFacilityFragment : Fragment() {
     // Connect to Facility viewModel
@@ -120,11 +116,9 @@ class EditFacilityFragment : Fragment() {
             .addOnSuccessListener {
                 var facName: String = it.get("name") as String
                 var address: String = it.get("address_street") as String
-                var postCode: String = it.get("address_postcode") as String
 
                 binding.txtEditFacilityName.setText(facName)
                 binding.txtEditFacilityAddress.setText(address)
-                binding.txtEditFacilityZipCode.setText(postCode)
 
 
             }.addOnFailureListener{
